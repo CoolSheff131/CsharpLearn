01ebe8e7c6a9e5589c15f9d5d21070a094a16c1e
CoolSheff131 <a.nikulsheev@gmail.com>
2021-06-17 20:53:29 +0800

    Добавлен новый раздел "крестики нолики".

--- CsharpLearn/Properties/Resources.Designer.cs
+++ CsharpLearn/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CsharpLearn.Properties {
+namespace snake.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CsharpLearn.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CsharpLearn.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("snake.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
