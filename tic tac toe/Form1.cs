1d0da45788907b6f868203e9e03015f33ff923d5
CoolSheff131 <a.nikulsheev@gmail.com>
2021-06-17 21:09:23 +0800

    Добавлено выключение кнопок при нажатий.

--- tic tac toe/Form1.cs
+++ tic tac toe/Form1.cs
@@ -12,14 +12,35 @@ namespace tic_tac_toe
 {
 	public partial class Form1 : Form
 	{
+		private int player;
 		public Form1()
 		{
 			InitializeComponent();
+			this.Height = 700;
+			this.Width = 900;
+			player = 1;
+			label1.Text = "Текущий ход: игрок 1";
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			MessageBox.Show(sender.GetType().GetProperty("Name").GetValue(sender).ToString());
+			switch (player)
+			{
+				case 1:
+					sender.GetType().GetProperty("Text").SetValue(sender, "x");
+					label1.Text = "Текущий ход: игрок 2";
+					player = 0;
+					break;
+				case 0:
+					sender.GetType().GetProperty("Text").SetValue(sender, "o");
+					label1.Text = "Текущий ход: игрок 1";
+					player = 1;
+					break;
+			}
+			sender.GetType().GetProperty("Enabled").SetValue(sender, false); 
+			//MessageBox.Show(sender.GetType().GetProperty("Name").GetValue(sender).ToString());
+
+
 		}
 	}
 }
