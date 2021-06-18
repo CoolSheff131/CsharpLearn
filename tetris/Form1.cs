6fd139640e682b0a4e591c7b28eadad5189ddd27
CoolSheff131 <a.nikulsheev@gmail.com>
2021-06-18 09:40:33 +0800

    Добавлена отрисовка сетки, класс фигура

---
+++ tetris/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tetris
+{
+	public partial class Form1 : Form
+	{
+		int size;
+		int[,] map = new int[8, 16];
+		public Form1()
+		{
+			InitializeComponent();
+			Init();
+		}
+		public void Init()
+		{
+			size = 25;
+			Invalidate();
+		}
+		public void DrawGrid(Graphics g)
+		{
+			for(int i = 0; i <= 16; i++)
+			{
+				g.DrawLine(Pens.Black, new Point(50, 50 + i * size), new Point(50 + 8 * size, 50 + i * size));
+			}
+			for (int i = 0; i <= 8; i++)
+			{
+				g.DrawLine(Pens.Black, new Point(50 + i * size, 50), new Point(50 + i * size, 50 + 16 * size));
+			}
+		}
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			DrawGrid(e.Graphics);
+		}
+	}
+}
