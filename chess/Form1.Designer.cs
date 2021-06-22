7791f0fdf7a7aa2af7e255da239da7552ab12834
CoolSheff131 <a.nikulsheev@gmail.com>
2021-06-22 11:39:05 +0800

    Добавлен функционал к фигурам и рестарт игры

--- chess/Form1.Designer.cs
+++ chess/Form1.Designer.cs
@@ -29,13 +29,25 @@ namespace chess
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(451, 33);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(105, 40);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Restart";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(432, 403);
+			this.ClientSize = new System.Drawing.Size(568, 403);
+			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -43,6 +55,8 @@ namespace chess
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button1;
 	}
 }
 
